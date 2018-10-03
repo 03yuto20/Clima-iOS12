6c6c3cd34d027a45db10dfc02b1e8ce8b97e7cff
Yuto Nakamura <yutonakamura0320@gmail.com>
2018-10-03 21:18:13 +0900

    2018/10/03

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -35,6 +35,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     locationManager.delegate = self
     locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
     locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingHeading()
     }
     
     
@@ -76,11 +77,21 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
